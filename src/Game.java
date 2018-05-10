6757aedf84e278136b0bb9f8da9440c83ceb229b
Aleksandr123456 <34124151+Aleksandr123456@users.noreply.github.com>
2018-05-11 01:49:03 +0300

    Update Game.java

--- src/Game.java
+++ src/Game.java
@@ -1,29 +1,11 @@
 import javax.swing.*;
-import java.awt.*;
 
 public class Game extends JFrame {
-
-    public Game() {
-
-        initUI();
-    }
-
-    private void initUI() {
-
-        add(new Board());
-
-        setTitle("MEGA IGRA");
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setSize(800, 375);
-        setLocationRelativeTo(null);
-        setVisible(true);
-    }
-
     public static void main(String[] args) {
-
-        EventQueue.invokeLater(() -> {
-            Game ex = new Game();
-            ex.setVisible(true);
-        });
+        JFrame frame = new JFrame("JustGame");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(1600,800);
+        frame.add(new Board(frame));
+        frame.setVisible(true);
     }
 }
